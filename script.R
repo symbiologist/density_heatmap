d881c95891597a8cba0a9ba90f050fcb35be132b
symbiologist <symbiologist@gmail.com>
2021-04-06 13:38:44 -0700

    Update with common scale

---
+++ script.R
@@ -0,0 +1,8 @@
+source <- '/media/data4/shared/ipsc_manuscript/analysis/perturbseq/output/density/normalized_density'
+dest <- '/media/data4/shared/density_heatmap/normalized_density'
+
+dir.create(dest, showWarnings = FALSE, recursive = TRUE)
+
+file.copy(from = list.files(source, full.names = TRUE),
+          to = dest,
+          overwrite = TRUE)
